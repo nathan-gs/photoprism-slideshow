ad25bf7ef000243f0d612f1351648a0ec70873e4
Nathan Bijnens <nathan@nathan.gs>
2023-07-27 17:23:19 +0200

    Flakes: split into smaller parts + Github tests

--- flake.nix
+++ flake.nix
@@ -19,84 +19,9 @@
         inherit system;
       };
 
-      app = sbt.lib.mkSbtDerivation {
-        inherit pkgs;
-
-        pname = "photoprism-slideshow";
-        version = "0.0.1";
-
-        depsSha256 = "sha256-nH5Ppa7/t4D1nePni4V2msPi+aoriz5V+b6/+pjoCy0=";
-        
-        buildInputs = [ ];
-        src = self;
-        nativeBuildInputs = [ ];
-        
-        buildPhase = ''
-          sbt assembly
-        '';
-
-        installPhase = "mkdir -p $out; cp target/scala-*/photoprism-slideshow-assembly-*.jar $out/photoprism-slideshow.jar";
-
-      };
-
     in {
-      packages.default = app;
-      nixosModules = {
-        photoprism-slideshow = { lib, pkgs, config, ... }:
-          let
-            cfg = config.services.photoprism-slideshow;
-          in
-          {
-            options = with lib; {
-              services.photoprism-slideshow = {
-                enable = mkOption {
-                  type = types.bool;
-                  default = false;
-                };
-
-                port = mkOption {
-                  type = types.int;
-                  default = 23234;
-                };
-
-                database = mkOption {
-                  type = types.str;
-                  default = "/var/lib/photoprism/index.db";
-                };
-
-                basePath = mkOption {
-                  type = types.str;
-                  default = "/slideshow";
-                };              
-
-                package = mkOption {
-                  type = types.package;
-                  default = self.outputs.packages."${pkgs.system}".photoprism-slideshow;
-                };              
-              };
-            };
-
-            config = with lib; mkIf cfg.enable {
-              systemd.services.photoprism-slideshow = {
-                enable = true;
-                path = [ pkgs.jre ];
-                environment = {
-                  SERVER_PORT = toString cfg.port;
-                  DATABASE = cfg.database;
-                  BASE_PATH = cfg.basePath;
-                };
-                script = ''
-                  java -jar ${cfg.package}/photoprism-slideshow.jar
-                '';
-              };
-
-              
-            };
-          };
-      };
+      packages.default = import ./package.nix { pkgs = pkgs; sbt = sbt; self = self; };
+      nixosModules.photoprism-slideshow = import ./module.nix { pkgs = pkgs; };
     }
   );
 }
-
-
-  
