47c9fb56b9bf647a69daa2a259049d51ffa339cb
Nathan Bijnens <nathan@nathan.gs>
2023-07-27 18:05:38 +0200

    Flake: back working

--- flake.nix
+++ flake.nix
@@ -18,58 +18,60 @@
       pkgs = import nixpkgs {
         inherit system;
       };
-
     in {
       packages.default = import ./package.nix { pkgs = pkgs; sbt = sbt; self = self; };
-      nixosModules.photoprism-slideshow = { lib, pkgs, config, ... }:
-        let
-          cfg = config.services.photoprism-slideshow;
-        in
-        {
-          options = with lib; {
-            services.photoprism-slideshow = {
-              enable = mkOption {
-                type = types.bool;
-                default = false;
-              };
+      #checks.build = self.packages.default;
+    }
+  ) // {
 
-              port = mkOption {
-                type = types.int;
-                default = 23234;
-              };
-
-              database = mkOption {
-                type = types.str;
-                default = "/var/lib/photoprism/index.db";
-              };
-
-              basePath = mkOption {
-                type = types.str;
-                default = "/slideshow";
-              };              
-
-              package = mkOption {
-                type = types.package;
-                default = self.outputs.packages."${pkgs.system}".photoprism-slideshow;
-              };              
-            };
+    nixosModules.photoprism-slideshow = { lib, pkgs, config, ... }:
+    let
+      cfg = config.services.photoprism-slideshow;
+    in
+    {
+      options = with lib; {
+        services.photoprism-slideshow = {
+          enable = mkOption {
+            type = types.bool;
+            default = false;
           };
 
-        config = with lib; mkIf cfg.enable {
-          systemd.services.photoprism-slideshow = {
-            enable = true;
-            path = [ pkgs.jre ];
-            environment = {
-              SERVER_PORT = toString cfg.port;
-              DATABASE = cfg.database;
-              BASE_PATH = cfg.basePath;
-            };
-            script = ''
-              java -jar ${cfg.package}/photoprism-slideshow.jar
-            '';
+          port = mkOption {
+            type = types.int;
+            default = 23234;
           };
+
+          database = mkOption {
+            type = types.str;
+            default = "/var/lib/photoprism/index.db";
+          };
+
+          basePath = mkOption {
+            type = types.str;
+            default = "/slideshow";
+          };              
+
+          package = mkOption {
+            type = types.package;
+            default = self.outputs.packages."${pkgs.system}".photoprism-slideshow;
+          };              
         };
       };
-    }
-  );
+
+      config = with lib; mkIf cfg.enable {
+        systemd.services.photoprism-slideshow = {
+          enable = true;
+          path = [ pkgs.jre ];
+          environment = {
+            SERVER_PORT = toString cfg.port;
+            DATABASE = cfg.database;
+            BASE_PATH = cfg.basePath;
+          };
+          script = ''
+            java -jar ${cfg.package}/photoprism-slideshow.jar
+          '';
+        };
+      };
+    };
+  };
 }
